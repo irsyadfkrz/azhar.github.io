447dcc2b51cec4f1282cd206b5fd9f46241997cb
mahapasaribu <maha.pasaribu@gmail.com>
2021-02-10 01:22:44 +0700

    portfolio

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// event ketika link di klik
+$('.page-scroll').on('click', function(e){
+
+	// ambil isi href
+	var tujuan = $(this).attr('href');
+	// tangkap elemen ybs
+	var elemenTujuan = $(tujuan);
+
+	// pindah scroll
+	$('html, body').animate({
+		scrollTop: elemenTujuan.offset().top - 50
+	}, 1000, 'easeInOutExpo');
+
+	e.preventDefault();
+
+});	
